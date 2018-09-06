f35d9de958610dd701038f9598499b5e79dcdfbb
Henocks <hr4574@naver.com>
2018-09-06 10:26:22 +0900

    fixed hoisting errors

--- src/grasschain/GrassChain.js
+++ src/grasschain/GrassChain.js
@@ -2,36 +2,56 @@ import React from 'react';
 import Web3 from 'web3';
 
 class GrassChain extends React.Component {
-	state = {
-		userAddress: 0
-	};
+  state = {
+    userAddress: 0
+  };
 
-	numberComma = (x) => {
-		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	}
+  numberComma = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 
-	componentWillMount() {
-		const ABI = [{ "constant": true, "inputs": [], "name": "GSCCount", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCHistory", "outputs": [{ "name": "history", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "GSCList", "outputs": [{ "name": "mainWallet", "type": "address" }, { "name": "value", "type": "uint256" }, { "name": "deposit", "type": "uint256" }, { "name": "Gchain_all", "type": "uint256" }, { "name": "status", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "verify", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCRatio", "outputs": [{ "name": "ratio", "type": "uint256[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "pay", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCAddr", "outputs": [{ "name": "Gchain", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "Gchain", "type": "address[]" }, { "name": "value", "type": "uint256" }, { "name": "ratio", "type": "uint256[]" }], "name": "createGSC", "outputs": [{ "name": "GSCID", "type": "uint256" }], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "abort", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCTimestamp", "outputs": [{ "name": "timestamp", "type": "uint256[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "anonymous": false, "inputs": [], "name": "aborted", "type": "event" }, { "anonymous": false, "inputs": [], "name": "purchaseConfirmed", "type": "event" }, { "anonymous": false, "inputs": [], "name": "paymentDone", "type": "event" }];
-		const contractAddr = "0xE8720CB8b80ffb4D93BeE736C624dc547603fc49";
-		const web3 = new Web3(Web3.givenProvider);
-		const GrassChain = new web3.eth.Contract(ABI, contractAddr);
-		const GSCID = 2;
 
-		console.log(Promise.resolve(getAddress()));
+  componentWillMount() {
+    const ABI = [{ "constant": true, "inputs": [], "name": "GSCCount", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCHistory", "outputs": [{ "name": "history", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "GSCList", "outputs": [{ "name": "mainWallet", "type": "address" }, { "name": "value", "type": "uint256" }, { "name": "deposit", "type": "uint256" }, { "name": "Gchain_all", "type": "uint256" }, { "name": "status", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "verify", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCRatio", "outputs": [{ "name": "ratio", "type": "uint256[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "pay", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCAddr", "outputs": [{ "name": "Gchain", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "Gchain", "type": "address[]" }, { "name": "value", "type": "uint256" }, { "name": "ratio", "type": "uint256[]" }], "name": "createGSC", "outputs": [{ "name": "GSCID", "type": "uint256" }], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "abort", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCTimestamp", "outputs": [{ "name": "timestamp", "type": "uint256[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "anonymous": false, "inputs": [], "name": "aborted", "type": "event" }, { "anonymous": false, "inputs": [], "name": "purchaseConfirmed", "type": "event" }, { "anonymous": false, "inputs": [], "name": "paymentDone", "type": "event" }];
+    const contractAddr = "0xE8720CB8b80ffb4D93BeE736C624dc547603fc49";
+    const web3 = new Web3(Web3.givenProvider);
+    const GrassChain = new web3.eth.Contract(ABI, contractAddr);
+    const GSCID = 2;
+    const getAddress = async () => {
+      const Adr = await web3.eth.getAccounts();
+      return Adr[0];
+    }
+    const getGSCData = async (GSCID) => {
+      const [GSCList, GSCRatio, GSCTimeStamp, GSCAddr] = await Promise.all(
+        [
+          GrassChain.methods.GSCList(GSCID).call(),
+          GrassChain.methods.getGSCRatio(GSCID).call(),
+          GrassChain.methods.getGSCTimestamp(GSCID).call(),
+          GrassChain.methods.getGSCAddr(GSCID).call()
+        ]);
+      console.log(GSCList);
+      console.log(GSCRatio);
+      console.log(GSCTimeStamp);
+      console.log(GSCAddr);
 
-		getAddress().then((address) => {
-			this.setState({
-				userAddress: address
-			})
-		});
+      return 0;
+    }
 
-		GrassChain.methods.GSCCount().call().then((result) => {
-			console.log(result);
-		});
+    console.log(Promise.resolve(getAddress()));
 
-		Promise.all([getAddress(), getGSCData(GSCID)]).then((address, GSCData) => {
-			console.log("User Address : " + address[0]);
-			console.log("GSC Data : " + GSCData);
+    getAddress().then((address) => {
+      this.setState({
+        userAddress: address
+      })
+    });
+
+    GrassChain.methods.GSCCount().call().then((result) => {
+      console.log(result);
+    });
+
+    Promise.all([getAddress(), getGSCData(GSCID)]).then((address, GSCData) => {
+      console.log("User Address : " + address[0]);
+      console.log("GSC Data : " + GSCData);
 
 			/*
 			GrassChain.methods.pay(GSCID).send(
@@ -47,60 +67,39 @@ class GrassChain extends React.Component {
 							console.log(reason);
 			});
 			*/
-		});
+    });
+  }
 
-		const getGSCData = async (GSCID) => {
-			const [GSCList, GSCRatio, GSCTimeStamp, GSCAddr] = await Promise.all(
-				[
-					GrassChain.methods.GSCList(GSCID).call(),
-					GrassChain.methods.getGSCRatio(GSCID).call(),
-					GrassChain.methods.getGSCTimestamp(GSCID).call(),
-					GrassChain.methods.getGSCAddr(GSCID).call()
-				]);
-			console.log(GSCList);
-			console.log(GSCRatio);
-			console.log(GSCTimeStamp);
-			console.log(GSCAddr);
+  createGSC() {
+    const ABI = [{ "constant": true, "inputs": [], "name": "GSCCount", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCHistory", "outputs": [{ "name": "history", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "GSCList", "outputs": [{ "name": "mainWallet", "type": "address" }, { "name": "value", "type": "uint256" }, { "name": "deposit", "type": "uint256" }, { "name": "Gchain_all", "type": "uint256" }, { "name": "status", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "verify", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCRatio", "outputs": [{ "name": "ratio", "type": "uint256[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "pay", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCAddr", "outputs": [{ "name": "Gchain", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "Gchain", "type": "address[]" }, { "name": "value", "type": "uint256" }, { "name": "ratio", "type": "uint256[]" }], "name": "createGSC", "outputs": [{ "name": "GSCID", "type": "uint256" }], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "abort", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCTimestamp", "outputs": [{ "name": "timestamp", "type": "uint256[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "anonymous": false, "inputs": [], "name": "aborted", "type": "event" }, { "anonymous": false, "inputs": [], "name": "purchaseConfirmed", "type": "event" }, { "anonymous": false, "inputs": [], "name": "paymentDone", "type": "event" }];
+    const contractAddr = "0xE8720CB8b80ffb4D93BeE736C624dc547603fc49";
+    let web3 = new Web3(Web3.givenProvider);
+    let GrassChain = new web3.eth.Contract(ABI, contractAddr);
 
-			return 0;
-		}
+    GrassChain.methods.pay(2).send(         // TO-DO : need to be refactored! - Dummy data.
+      {
+        from: '0x8CAd9B4941aAfb67b5A5e6DeA657Db2d4ea7b757',
+        to: contractAddr,
+        value: web3.utils.toWei('0.01', 'ether')
+      }).then((result) => {
+        console.log("Sent done!");
+        console.log(result);
+      }, (reason) => {
+        console.log("Error!");
+        console.log(reason);
+      });
+  }
 
-		const getAddress = async () => {
-			const Adr = await web3.eth.getAccounts();
-			return Adr[0];
-		}
-	}
+  render() {
+    return (
+      <div>
+        <h1>GrassChain Component Works!</h1>
+        <h2>Ethereum Wallet Address : {this.state.userAddress} </h2>
+        <button onClick={this.createGSC}>Create GSC!</button>
+      </div>
+    );
 
-	createGSC() {
-		const ABI = [{ "constant": true, "inputs": [], "name": "GSCCount", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCHistory", "outputs": [{ "name": "history", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "GSCList", "outputs": [{ "name": "mainWallet", "type": "address" }, { "name": "value", "type": "uint256" }, { "name": "deposit", "type": "uint256" }, { "name": "Gchain_all", "type": "uint256" }, { "name": "status", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "verify", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCRatio", "outputs": [{ "name": "ratio", "type": "uint256[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "pay", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCAddr", "outputs": [{ "name": "Gchain", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "Gchain", "type": "address[]" }, { "name": "value", "type": "uint256" }, { "name": "ratio", "type": "uint256[]" }], "name": "createGSC", "outputs": [{ "name": "GSCID", "type": "uint256" }], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "abort", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCTimestamp", "outputs": [{ "name": "timestamp", "type": "uint256[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "anonymous": false, "inputs": [], "name": "aborted", "type": "event" }, { "anonymous": false, "inputs": [], "name": "purchaseConfirmed", "type": "event" }, { "anonymous": false, "inputs": [], "name": "paymentDone", "type": "event" }];
-		const contractAddr = "0xE8720CB8b80ffb4D93BeE736C624dc547603fc49";
-		let web3 = new Web3(Web3.givenProvider);
-		let GrassChain = new web3.eth.Contract(ABI, contractAddr);
-
-		GrassChain.methods.pay(2).send(         // TO-DO : need to be refactored! - Dummy data.
-		{
-			from: '0x8CAd9B4941aAfb67b5A5e6DeA657Db2d4ea7b757',
-			to: contractAddr,
-			value: web3.utils.toWei('0.01', 'ether')
-		}).then((result) => {
-			console.log("Sent done!");
-			console.log(result);
-		}, (reason) => {
-			console.log("Error!");
-			console.log(reason);
-		});
-	}
-
-	render() {
-		return (
-			<div>
-				<h1>GrassChain Component Works!</h1>
-				<h2>Ethereum Wallet Address : {this.state.userAddress} </h2>
-				<button onClick={this.createGSC}>Create GSC!</button>
-			</div>
-		);
-
-	}
+  }
 
 }
 
--- src/grasschain/GrassCoin.js
+++ src/grasschain/GrassCoin.js
@@ -1,57 +1,59 @@
 import React from 'react';
 import Web3 from 'web3';
 
-class GrassCoin extends React.Component{
+class GrassCoin extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {
-            userAddress: 0,
-            GRCBalance: 0
-        };
+  constructor() {
+    super();
+    this.state = {
+      userAddress: 0,
+      GRCBalance: 0
+    };
+  }
+
+  componentWillMount() {
+    const GABI = [{ "constant": true, "inputs": [], "name": "name", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "vaultID", "type": "uint256" }], "name": "terminateGRC", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "vaultList", "outputs": [{ "name": "master", "type": "address" }, { "name": "value", "type": "uint256" }, { "name": "total", "type": "uint256" }, { "name": "timestamp", "type": "uint256" }, { "name": "status", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "name": "", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_value", "type": "uint256" }], "name": "burn", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_value", "type": "uint256" }], "name": "depositGRC", "outputs": [{ "name": "success", "type": "bool" }, { "name": "vID", "type": "uint256" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "burnFrom", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transfer", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "vaultID", "type": "uint256" }], "name": "stakeGRC", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }, { "name": "_extraData", "type": "bytes" }], "name": "approveAndCall", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }, { "name": "", "type": "address" }], "name": "allowance", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [{ "name": "initialSupply", "type": "uint256" }, { "name": "tokenName", "type": "string" }, { "name": "tokenSymbol", "type": "string" }], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "from", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Burn", "type": "event" }];
+    const grcAddr = "0x1179cf51c590794b0897586bc83a8d82ef8a243b";
+    const web3 = new Web3(Web3.givenProvider);
+    const GrassCoin = new web3.eth.Contract(GABI, grcAddr);
+
+    const getAddress = async () => {
+      const Adr = await web3.eth.getAccounts();
+      return Adr[0];
     }
 
-    componentWillMount() {
-        const GABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"vaultID","type":"uint256"}],"name":"terminateGRC","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"vaultList","outputs":[{"name":"master","type":"address"},{"name":"value","type":"uint256"},{"name":"total","type":"uint256"},{"name":"timestamp","type":"uint256"},{"name":"status","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"depositGRC","outputs":[{"name":"success","type":"bool"},{"name":"vID","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_value","type":"uint256"}],"name":"burnFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"vaultID","type":"uint256"}],"name":"stakeGRC","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"tokenSymbol","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"}];
-        const grcAddr = "0x1179cf51c590794b0897586bc83a8d82ef8a243b";
-        const web3 = new Web3(Web3.givenProvider);
-        const GrassCoin = new web3.eth.Contract(GABI, grcAddr);
+    getAddress().then((address) => {
+      GrassCoin.methods.balanceOf(address).call().then((balance) => {
+        this.setState({
+          userAddress: address,
+          GRCBalance: balance
+        })
+      })
+    });
 
-        getAddress().then((address) => {
-            GrassCoin.methods.balanceOf(address).call().then((balance) => {
-                this.setState({
-                    userAddress: address,
-                    GRCBalance: balance
-                })
-            })
-        });
+    //getAddress and create purchase GSC
+    Promise.all([getAddress()]).then((address) => {
+      console.log(address[0]);
+      GrassCoin.methods.balanceOf(address[0]).call().then((result) => {
+        console.log(result);
+      });
+    });
 
-        //getAddress and create purchase GSC
-        Promise.all([getAddress()]).then((address) => {
-            console.log(address[0]);
-            GrassCoin.methods.balanceOf(address[0]).call().then((result) => {
-                console.log(result);
-            });
-        });
 
-        const getAddress = async () => {
-            const Adr = await web3.eth.getAccounts();
-            return Adr[0];
-        }
+  }
+  render() {
+    const numberComma = (x) => {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
-    render(){
-        const numberComma = (x) => {
-            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        }
 
-        return(
-            <div>
-                <h1><br /><br />GrassCoin Component Works!</h1>
-                <h2>Ethereum Wallet Address : {this.state.userAddress}</h2>
-                <h2>GRC Balance : {numberComma(this.state.GRCBalance)} GRC<br /><br /></h2>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <h1><br /><br />GrassCoin Component Works!</h1>
+        <h2>Ethereum Wallet Address : {this.state.userAddress}</h2>
+        <h2>GRC Balance : {numberComma(this.state.GRCBalance)} GRC<br /><br /></h2>
+      </div>
+    );
+  }
 }
 
 export default GrassCoin;
